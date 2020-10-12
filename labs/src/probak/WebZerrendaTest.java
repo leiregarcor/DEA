92220a1a85037cab67635602e14954c58e10102b
leire <leire@DESKTOP-98AEAF4>
2020-10-12 18:31:40 +0200

    jhkg

---
+++ labs/src/probak/WebZerrendaTest.java
@@ -0,0 +1,52 @@
+package probak;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import packlEnuntziatu1.WebZerrenda;
+
+public class WebZerrendaTest {
+	private static WebZerrenda wz;
+	@Before
+	public void setUp() throws Exception {
+		wz= WebZerrenda.getNireWebZerrenda();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIndexFitxeroaKargatu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArcFitxeroaKargatu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBilatuUrl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGehitu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEzabatu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListaOrdenatuta() {
+		fail("Not yet implemented");
+	}
+
+}
