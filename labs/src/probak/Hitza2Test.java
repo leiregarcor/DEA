96d36cb562bccdf2cb54686af7953d00d0b958bb
leire <leiregarciaupv@gmail.com>
2020-10-24 14:11:12 +0200

    he cambiado el test de hitza del enuntziatu 1 pq estaba hecha con una version vieja de test

---
+++ labs/src/probak/Hitza2Test.java
@@ -0,0 +1,49 @@
+package probak;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+//import static org.junit.Assert.*;
+
+import packlEnuntziatu1.GakoHitzZerrenda;
+import packlEnuntziatu1.Hitza;
+import packlEnuntziatu1.WebZerrenda;
+
+import java.io.File;
+
+public class Hitza2Test {
+    private static  GakoHitzZerrenda ghz;
+    private static WebZerrenda wz;
+    private Hitza h;
+    private File wordsFitxeroa ;
+    private File webIndexFitxeroa ;
+
+    @Before
+    public void setUp() throws Exception {
+        wz= WebZerrenda.getNireWebZerrenda();
+        ghz = GakoHitzZerrenda.getNireGakoHitzZerrenda();
+
+        webIndexFitxeroa = new File ("resources\\index.txt");
+        wordsFitxeroa = new File ("resources\\words.txt");
+
+        ghz.fitxeroaKargatu(wordsFitxeroa);
+        wz.indexFitxeroaKargatu(webIndexFitxeroa);
+
+        h= ghz.bilatuHitza("sport");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        wz=null;
+        ghz= null;
+        webIndexFitxeroa = null;
+        wordsFitxeroa = null;
+        h = null;
+    }
+
+    @Test
+    public void gakoWebInprimatu() {
+        h.gakoWebInprimatu();
+    }
+}
