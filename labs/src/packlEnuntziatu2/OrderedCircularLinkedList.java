f24d332b8651a893658c010fb8fc208c7f47ede7
Usuario <Usuario@AITOR>
2020-11-01 11:34:14 +0100

    Ordered

--- labs/src/packlEnuntziatu2/OrderedCircularLinkedList.java
+++ labs/src/packlEnuntziatu2/OrderedCircularLinkedList.java
@@ -1,6 +1,6 @@
 package packlEnuntziatu2;
 
-public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T> {
+public class OrderedCircularLinkedList<T extends Comparable<T>> extends CircularLinkedList<T> implements OrderedListADT<T> {
 	
 	
 	public OrderedCircularLinkedList(String pDeskr)
@@ -10,55 +10,74 @@ public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implemen
 	}
 
 	public void add(T elem) {
-		Node<T> unekoa= last.next;
-		Node<T> aurrekoa= last;
+		Comparable <T> generiko = (Comparable <T>) elem;
 		boolean txertatuta=false;
-		Node<T> berria= new Node<T>(elem);
-		while(aurrekoa!=last && !txertatuta) {
-			if(elem.toString().compareTo(unekoa.elem.toString())<=0) {
-				berria.next=unekoa;
-				aurrekoa.next=berria;
-				txertatuta=true;
-			}else {
-				aurrekoa=unekoa;
-				unekoa=unekoa.next;
+		Node<T> berria= new Node<T>((T) generiko);
+		if(last==null) {
+			berria.next=berria;
+			last=berria;
+			count ++;
+		}else {
+			Node<T> unekoa= last.next;
+			Node<T> aurrekoa= null;
+			while(aurrekoa!=last && !txertatuta) {
+				if(elem.compareTo((T) unekoa.elem)<=0) {
+					if(unekoa==last.next) {
+						last.next=berria;
+						berria.next=unekoa;
+					}else {
+						berria.next=unekoa;
+						aurrekoa.next=berria;
+					}	
+					txertatuta=true;
+					count ++;
+				}else {
+					aurrekoa=unekoa;
+					unekoa=unekoa.next;
+				}
+			}
+			if(!txertatuta) {
+				berria.next=last.next;
+				last.next=berria;
+				last=berria;
 			}
 		}
-		if(!txertatuta) {
-			berria.next=last.next;
-			last.next=berria;
-			last=berria;
-		}	
 	}
 
 	public void merge(OrderedCircularLinkedList<T> z) {
 		if(z.last==null) {
 			
 		}else {
-			Node<T> l1= last.next;
-			Node<T> l1aur= last;
-			Node<T> l2= z.last.next;
-			int s= this.size();
-			int konp;
-			while(this.size()!=s+z.size()){
-				Node<T> berria= new Node<T>(l2.elem);
-				konp= l2.elem.toString().compareTo(l1.elem.toString());
-				if(konp<=0) {
-					berria.next=l1;
-					l1aur.next=berria;
-					l2=l2.next;
-					l1=berria;
-				}else {
-					if(l1==last) {
-						berria.next=last.next;
-						l1.next=berria;
-						last=berria;
+			if(last==null) {
+				last=z.last;
+			}else {
+				Node<T> l1= last.next;
+				Node<T> l1aur= last;
+				Node<T> l2= z.last.next;
+				int s= this.size();
+				int konp;
+				while(this.size()!=s+z.size()){
+					Node<T> berria= new Node<T>(l2.elem);
+					konp= l2.elem.compareTo((T) l1.elem);
+					if(konp<=0) {
+						berria.next=l1;
+						l1aur.next=berria;
+						l2=l2.next;
+						l1=berria;
+						count ++;
+					}else {
+						if(l1==last) {  		//lista amaitu da eta l2 l1 baino handiagoa da
+							berria.next=last.next;
+							l1.next=berria;
+							last=berria;
+							count ++;
+						}
+						l1aur=l1;
+						l1=l1.next;
 					}
-					l1aur=l1;
-					l1=l1.next;
 				}
-			}
 		
+			}
 		}
 	}	
 	
--- labs/src/packlEnuntziatu2/OrderedListADT.java
+++ labs/src/packlEnuntziatu2/OrderedListADT.java
@@ -1,6 +1,6 @@
 package packlEnuntziatu2;
 
-public interface OrderedListADT<T> extends ListADT<T> {
+public interface OrderedListADT<T extends Comparable<T>> extends ListADT<T> {
 	public void add (T elem);
 	public void merge (OrderedCircularLinkedList<T> z);
 }
