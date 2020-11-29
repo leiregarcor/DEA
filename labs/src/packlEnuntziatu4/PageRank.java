66a2741ca80f71517b8fee4b3d9ae191c1e880e6
leire <leiregarciaupv@gmail.com>
2020-11-30 00:05:53 +0100

    4. eginkizuna

---
+++ labs/src/packlEnuntziatu4/PageRank.java
@@ -0,0 +1,34 @@
+package packlEnuntziatu4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PageRank {
+
+    public HashMap<String, Double> pageRank() {
+        //POST: emaitza web-orri zerrendaren web-orri bakoitzaren PageRank algoritmoaren balioa da
+        HashMap<String, Double> pg = new HashMap<>();
+
+        return pg;
+    }
+
+
+    public class Bikote {
+        String web;
+        Double pageRank;
+    }
+
+    public ArrayList<Bikote> bilatzailea(String gakoHitz) {
+        // Post: Emaitza emandako gako-hitza duten web-orrien zerrenda da, bere pagerank - aren arabera handienetik
+        // txikienera ordenatuta (hau da, lehenengo posizioetan pagerank handiena duten web - orriak agertuko dira)
+        ArrayList<Bikote> ema= new ArrayList<>();
+        return ema;
+    }
+
+    public ArrayList<Bikote> bilatzailea(String gakoHitz1, String gakoHitz2){
+        // Post: Emaitza emandako gako-hitzak dituzten web-orrien zerrenda da, bere pagerank-aren arabera handienetik
+        //  txikienera ordenatuta(hau da, lehenengo posizioetan pagerank handiena duten web-orriak agertuko dira)
+        ArrayList<Bikote> ema= new ArrayList<>();
+        return ema;
+    }
+}
