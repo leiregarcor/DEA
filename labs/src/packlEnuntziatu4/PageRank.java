c7448d6d92c68d6ee1597cd6753590d79c27462d
Aitor <aitorsanjoseheras@gmail.com>
2020-12-13 11:44:16 +0100

    PageRank hecho

--- labs/src/packlEnuntziatu4/PageRank.java
+++ labs/src/packlEnuntziatu4/PageRank.java
@@ -1,9 +1,14 @@
 package packlEnuntziatu4;
 
+import packlEnuntziatu1.GakoHitzZerrenda;
+import packlEnuntziatu1.Hitza;
+import packlEnuntziatu1.Web;
 import packlEnuntziatu1.WebZerrenda;
+import packlEnuntziatu2.OrderedCircularLinkedList;
 import packlEnuntziatu3.Graph;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 public class PageRank {
@@ -56,39 +61,64 @@ public class PageRank {
         this.hasieratuPageRank();
         double dif=1;
         String lag;
+        int aux;
         double balioa;
         while(dif>0.0001){
+            dif=0;
             zahar=berri;
             berri=new HashMap<String, Double>();
             for (int i=0; i<g.getKeys().length; i++){
                 balioa=0;
                 lag=g.getKeys()[i];
                 for (int k=0; k<heldutakoakList[i].size(); k++){
-                    balioa=balioa+ (zahar.get(heldutakoakList[i].get(k))/g.getAdjList()[heldutakoakList[i].get(k)].size());
+                    aux=heldutakoakList[i].get(k);
+                    balioa=balioa+ ((zahar.get(aux)/g.getAdjList()[aux].size())*d);
                     //lag=heldutakoakList[i].get(k);
                 }
-                berri.put(lag,)
+                balioa=balioa+((1-d)/n);
+                berri.put(lag,balioa);
+                dif=dif+Math.abs(berri.get(lag)-zahar.get(lag));
             }
 
-
-
         }
-
-
-
         return berri;
     }
 
 
-    public class Bikote {
+    public class Bikote implements Comparable<Bikote> {
         String web;
         Double pageRank;
+
+        @Override
+        public int compareTo(Bikote o) {
+            if(pageRank==o.pageRank) {
+                return 0;
+            }else {
+                if (pageRank<o.pageRank){
+                    return -1;
+                }else {
+                    return 1;
+                }
+            }
+        }
     }
 
     public ArrayList<Bikote> bilatzailea(String gakoHitz) {
         // Post: Emaitza emandako gako-hitza duten web-orrien zerrenda da, bere pagerank - aren arabera handienetik
         // txikienera ordenatuta (hau da, lehenengo posizioetan pagerank handiena duten web - orriak agertuko dira)
+        OrderedCircularLinkedList<Bikote> lista= new OrderedCircularLinkedList<>("Page ranken lista");
         ArrayList<Bikote> ema= new ArrayList<>();
+        Hitza h= GakoHitzZerrenda.getNireGakoHitzZerrenda().bilatuHitza(gakoHitz);
+        for(int i=0; i<h.getWebLista().size(); i++){
+            Web w= h.getWebLista().get(i);
+            Bikote b= new Bikote();
+            b.web=w.getUrlWeb();
+            b.pageRank= berri.get(w.getUrlWeb());
+            lista.add(b);
+        }
+        while (!lista.isEmpty()){
+            ema.add(lista.removeFirst());
+        }
         return ema;
     }
 
