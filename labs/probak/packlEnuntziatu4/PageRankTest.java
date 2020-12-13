2ac5f2ef55edbe4212f3f716a75e4b4e7f75a3e8
leire <leiregarciaupv@gmail.com>
2020-12-13 19:08:51 +0100

    uml

---
+++ labs/probak/packlEnuntziatu4/PageRankTest.java
@@ -0,0 +1,30 @@
+package packlEnuntziatu4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PageRankTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void pageRank() {
+    }
+
+    @Test
+    public void bilatzailea() {
+    }
+
+    @Test
+    public void testBilatzailea() {
+    }
+}
